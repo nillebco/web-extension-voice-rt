6f452d70243fc292e911ce861d2286bdbbf5621b
Ivo Bellin Salarin <ivo@nilleb.com>
2025-02-25 18:58:17 +0100

    fix: don't hide the original document

--- reader.js
+++ reader.js
@@ -2,7 +2,7 @@ async function generateReaderContent() {
     const documentClone = document.cloneNode(true);
 
     try {
-        return new Readability(document).parse()
+        return new Readability(documentClone).parse()
     } catch (error) {
         console.error("Error generating reader content: ", error);
         return { error, content: `<p>Error generating reader content!</p><pre>${error}</pre>` };
